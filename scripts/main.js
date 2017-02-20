9d7734f5857178b94a81565f18386cc4a3b79110
Katarina Berg <katarina_berg@hotmail.com>
2017-02-20 11:13:12 -0500

    Modified javasript
    - Focus on the other input field if current reached maxlength but other has not
    - Count nbr of days between the two dates if both input fields reached maxlength

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,62 @@
 
-var myHTML = document.querySelector("html");
-myHTML.addEventListener("keydown", function (e) {
-  if (e.keyCode === 13) {
-    alert("Wiaa!");
+var firstInput = document.getElementById("from-date");
+var secondInput = document.getElementById("to-date");
+
+firstInput.onkeyup = function(e) {
+  var target = e.target;
+  if (maxLengthAchieved(target)) {
+    if (maxLengthAchieved(secondInput)) {
+      countDown(target, secondInput);
+    } else {
+      secondInput.focus();
+    }
   }
-});
+}
+
+secondInput.onkeyup = function(e) {
+  var target = e.target;
+  if (maxLengthAchieved(target)) {
+    if (maxLengthAchieved(firstInput)) {
+      countDown(firstInput, target);
+    } else {
+      firstInput.focus();
+    }
+  }
+}
+
+function maxLengthAchieved(element) {
+  var maxLength = parseInt(element.getAttribute("maxlength"), 10); //element.maxlength
+  var currentLength = element.value.length;
+  return currentLength >= maxLength;
+}
+
+function getDateInUtc(dateString) {
+  var year = dateString.value.substr(0,4);
+  var month = dateString.value.substr(4,2);
+  var day = dateString.value.substr(6,2);
+  var date = new Date(year, month - 1, day);
+
+  var valid = date && (date.getMonth() + 1 === parseInt(month, 10));
+  if (!valid) {
+    alert("Oops invalid date!");
+    return null;
+  } else {
+    date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+    return date;
+  }
+}
+
+function countDown(from, to) {
+  var fromDate = getDateInUtc(from);
+  if (fromDate === null) return;
+  var fromDateMilliseconds = fromDate.getTime();
+
+  var toDate = getDateInUtc(to);
+  if (toDate === null) return;
+  var toDateMilliseconds = toDate.getTime();
+
+  var millisecondsPerDay = 1000*60*60*24;
+  var diff = (toDateMilliseconds - fromDateMilliseconds) / millisecondsPerDay;
+
+  alert(diff + "  days left!");
+}
